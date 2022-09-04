b8360c4d5f42c450c1a1c4c28700021e0b7f0113
Mayur Upadhayay <mayur123a@gmail.com>
2022-09-04 22:47:27 +0530

    feat: Adds a minimalistic Authorization server

--- owner-password/auth_server.php
+++ owner-password/auth_server.php
@@ -9,3 +9,34 @@ $scopeRepository = new ScopeRepository(); // instance of ScopeRepositoryInterfac
 $accessTokenRepository = new AccessTokenRepository(); // instance of AccessTokenRepositoryInterface
 $userRepository = new UserRepository(); // instance of UserRepositoryInterface
 $refreshTokenRepository = new RefreshTokenRepository(); // instance of RefreshTokenRepositoryInterface
+
+// Path to public and private keys
+$privateKey = realpath('./keys/private.key');
+$privateKey = 'file://' . $privateKey;
+
+// generate using base64_encode(random_bytes(32))
+$encryptionKey = base64_encode(random_bytes(32));
+
+// Setup the authorization server
+$server = new \League\OAuth2\Server\AuthorizationServer(
+    $clientRepository,
+    $accessTokenRepository,
+    $scopeRepository,
+    $privateKey,
+    $encryptionKey
+);
+
+// Specifies the grant type
+$grant = new \League\OAuth2\Server\Grant\PasswordGrant(
+     $userRepository,
+     $refreshTokenRepository
+);
+
+// sets refresh token expiry to 1 month
+$grant->setRefreshTokenTTL(new \DateInterval('P1M'));
+
+// Enable the password grant on the server
+$server->enableGrantType(
+    $grant,
+    new \DateInterval('PT1H') // access tokens will expire after 1 hour
+);
